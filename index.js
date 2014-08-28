f64c2a4b8bb8c9075e0586e96e49c9043ca75604
Abraham Levitan <abe@ore.belkin>
2014-08-28 16:09:50 -0400

    more fiddlywinks

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/static', express.static(__dirname + '/static'));
 app.get('/', function(req, res) {
     console.log('Incoming connection');
     mongo.Db.connect(mongoURI, function(err, db){
-	console.log(db);
+	console.log(err);
 	db.collection('loc', function(er, loc){
 	    loc.findOne({name: 'car'}, function(err, car){
 		res.render('layout', {loc: car.loc});
