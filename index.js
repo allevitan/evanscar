fb4425f968ccaada61ed2ee5e6fef049e80db82b
Abraham Levitan <abe@ore.belkin>
2014-08-28 16:21:52 -0400

    attempt to quash the occasional crashing behavior

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use('/static', express.static(__dirname + '/static'));
 
-app.get('/', function(req, res) {
-    mongo.Db.connect(mongoURI, function(err, db){
-	db.collection('loc', function(er, loc){
+mongo.Db.connect(mongoURI, function(err, db){
+    db.collection('loc', function(er, loc){
+
+	app.get('/', function(req, res) {
 	    loc.findOne({name: 'car'}, function(err, car){
 		if (car){
 		    res.render('layout', {loc: car.loc});
@@ -23,24 +24,22 @@ app.get('/', function(req, res) {
 		};
 	    });
 	});
-    });
-})
-
-io.on('connection', function(socket){
-    console.log('A user connected');
-    socket.on('update', function(id){
-	mongo.Db.connect(mongoURI, function(err, db){
-	    db.collection('loc', function(er, loc){
+	
+	io.on('connection', function(socket){
+	    console.log('A user connected');
+	    socket.on('update', function(id){
 		loc.update({name: 'car'}, {name: 'car', loc: id}, {upsert: true}, function(){
 		    io.emit('update', id);
 		    console.log('location updated to ' + id);
 		});
 	    });
 	});
+
+
+	http.listen(app.get('port'), function() {
+	    console.log("Evan's car is running at localhost:" + app.get('port'));
+	})
+
     });
-});
-
-
-http.listen(app.get('port'), function() {
-  console.log("Evan's car is running at localhost:" + app.get('port'));
 })
+
